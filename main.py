5e020d27ab5157accc5bb21636f923f1cab544f9
Erich Malberg <emalberg@live.com>
2022-06-20 22:50:42 -0600

    Creating UI Testing Branch -Added basic UI

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-from PySide6 import QtCore, QtWidgets, QtGui
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtCore import QFile
+from ui_mainwindow import Ui_Form
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec())
