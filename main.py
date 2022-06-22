ddb798bad677a817a6c62b48b9b581b75a9cf884
Erich Malberg <emalberg@live.com>
2022-06-21 23:27:33 -0600

    -Created a UI class file -added two dark themes to work with -customized Material Dark theme(need to do same to Elegant Dark theme) - Added Basics Label's and Form information.

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import sys
+
+from PySide6 import QtCore, QtGui
 from PySide6.QtWidgets import QApplication, QMainWindow
-from PySide6.QtCore import QFile
-from ui_mainwindow import Ui_Form
+from ui_mainwindow import Ui_MainWindow
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        self.ui = Ui_Form()
+        self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    app.setStyle('Fusion')
+
+    with open("themes/MaterialDark.qss", "r") as f:
+        _style = f.read()
+        app.setStyleSheet(_style)
 
     window = MainWindow()
     window.show()
